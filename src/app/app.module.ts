89df85123b9e6d9e6122d52b73f5baa54b5f6b66
Eric Eslinger <eric.eslinger@gmail.com>
2017-03-06 21:40:18 -0800

    just fiddling, really

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { DiceComponent } from './dice/dice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiceComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    MaterialModule,
     HttpModule
   ],
   providers: [],
