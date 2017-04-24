37e5540638daa38be79bbe4702bd450ffdb5fd18
Eric Eslinger <eric.eslinger@gmail.com>
2017-04-24 16:58:40 -0700

    just fiddling with observable streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -6,12 +6,10 @@ import { Subject } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('pm') plusMinus;
   title = 'app works!';
-  dice$ = new Subject();
-  rollStream = this.dice$.asObservable();
-  onClick() {
-    this.dice$.next(Math.floor(Math.random() * 6) + 1);
-    console.log('cleek');
+  ngAfterViewInit() {
+    this.plusMinus.count$.subscribe(v => console.log('SUB', v));
   }
 }
--- src/app/plus-minus/plus-minus.component.ts
+++ src/app/plus-minus/plus-minus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Input, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MdButton } from '@angular/material';
 
@@ -11,30 +11,22 @@ export class PlusMinusComponent implements OnInit {
 
   @ViewChild('plus') plus: MdButton;
   @ViewChild('minus') minus: MdButton;
-
-  currentValue: number;
+  @Input() maxValue: number;
+  @Input() minValue: number;
+  @Output() count: Observable<number>;
   count$: Observable<number>;
 
-  constructor() {
-    this.currentValue = 0;
-  }
+  // constructor() { }
 
   ngOnInit() {
     this.count$ = Observable.merge(
-      // decrement
-      Observable.fromEvent<MouseEvent>(this.plus._getHostElement(), 'click')
-        .map(() => ({ delta : +1 }) ),
-
-      // increment
-      Observable.fromEvent<MouseEvent>(this.minus._getHostElement(), 'click')
-        .map(() => ({ delta : -1 }) ),
-
-      // reset
-      // Observable.fromEvent($('#res'), 'click')
-      //   .map(() => ({ return {reset : true)))
-    ) // merge
-    .scan((current, value) => Math.max(0, current + value.delta), 0)
-    .startWith(0);
+      Observable.fromEvent<MouseEvent>(this.plus._getHostElement(), 'click') .mapTo(+1),
+      Observable.fromEvent<MouseEvent>(this.minus._getHostElement(), 'click') .mapTo(-1)
+    )
+    .scan((current, value) => Math.min(this.maxValue, Math.max(this.minValue, current + value)), this.minValue)
+    .distinctUntilChanged()
+    .share()
+    .startWith(this.minValue);
   }
 
 }
