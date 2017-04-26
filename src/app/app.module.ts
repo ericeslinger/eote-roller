aa599567ce60b794b2a2acb59dc5b8d124e381d3
Eric Eslinger <eric.eslinger@gmail.com>
2017-04-25 23:47:16 -0700

    better layouts with mdl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { PlusMinusComponent } from './plus-minus/plus-minus.component';
 import { DiePanelComponent } from './fancy-dice/die-panel.component';
-import { DiceBagComponent } from './dice-bag/dice-bag.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlusMinusComponent,
     DiePanelComponent,
-    DiceBagComponent,
   ],
   imports: [
     BrowserModule,
